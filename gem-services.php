fed6c5b5b5bf579fd1639cd6506e1ea25fbfd965
Daniel Guerrero <daniel.guerrero.1000@gmail.com>
2022-11-11 11:00:32 -0600

    main

---
+++ gem-services.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  Plugin Name: gem-services
+  Plugin URI: https://github.com/Trifoia/wordpress-shaka-player
+  description: WordPress plugin that allows embedding of the gem-services via shortcode
+  Version: 1.0.0
+  Author: comunicacion social
+  Author URI: https://trifoia.com
+*/
+
